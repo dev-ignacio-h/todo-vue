13fd8f8b197f738ac36bbe36a1e32bc691e3b1d2
ignacio <dev.ignacio.h@gmail.com>
2020-11-29 12:24:14 -0300

    first commit:
    components new task, task and task grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1>Tareas</h1>
+		<NewTask @taskAdded="addTask" />
+		<TaskGrid :tasks="tasks" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskGrid from './components/TaskGrid'
+import NewTask from './components/NewTask'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	components: { TaskGrid, NewTask },
+	data() {
+		return {
+			tasks: [
+				{ name: 'Lavar losa', pending: false },
+				{ name: 'Comprar camisas', pending: true },
+			]
+		}
+	},
+	methods: {
+		addTask(task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			// if(reallyNew) {
+			// 	this.tasks.push({
+			// 		name: task.name,
+			// 		pending: task.pending || true
+			// 	})
+			// }
+			reallyNew && this.tasks.push({
+				name: task.name,
+				pending: task.pending || true
+			})
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
 </style>
